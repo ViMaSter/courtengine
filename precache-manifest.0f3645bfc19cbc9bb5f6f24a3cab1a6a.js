b87f7672daa66c17745569b4af16387f948d5939
sudovickili <sudovickili@gmail.com>
2020-02-03 20:05:16 -0800

    Updates

---
+++ precache-manifest.0f3645bfc19cbc9bb5f6f24a3cab1a6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec83d172d3fb5e96ddec5043104289d",
+    "url": "/courtengine/demo/courtengine-script-editor/index.html"
+  },
+  {
+    "revision": "b93891df744e8b927e74",
+    "url": "/courtengine/demo/courtengine-script-editor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e3a1fa67c52c2e2a7ed2",
+    "url": "/courtengine/demo/courtengine-script-editor/static/js/2.91447e2a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/courtengine/demo/courtengine-script-editor/static/js/2.91447e2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93891df744e8b927e74",
+    "url": "/courtengine/demo/courtengine-script-editor/static/js/main.fce45c61.chunk.js"
+  },
+  {
+    "revision": "f127c7b7410863df318a",
+    "url": "/courtengine/demo/courtengine-script-editor/static/js/runtime-main.21ff31c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/courtengine/demo/courtengine-script-editor/static/media/logo.5d5d9eef.svg"
+  }
+]);
